9398d377b3ebe6459b7e5dcb461e17a13804ee21
Martin Kauppinen <martin.kauppinen@gmail.com>
2022-12-03 08:40:13 +0100

    Add day 03

---
+++ src/day03.rs
@@ -0,0 +1,68 @@
+use itertools::Itertools;
+
+fn to_priority(c: char) -> usize {
+    assert!(c.is_ascii_alphabetic());
+
+    let ascii = c as usize;
+    if c.is_lowercase() {
+        ascii - ('a' as usize) + 1
+    } else {
+        ascii - ('A' as usize) + 1 + 26
+    }
+}
+
+#[aoc(day03, part1)]
+pub fn solve_part1(input: &str) -> usize {
+    let rucksacks = input.lines().map(|line| line.split_at(line.len() / 2));
+
+    let mut sum = 0;
+    for (a, b) in rucksacks {
+        for c in a.chars() {
+            if b.contains(c) {
+                sum += to_priority(c);
+                break;
+            }
+        }
+    }
+    sum
+}
+
+#[aoc(day03, part2)]
+pub fn solve_part2(input: &str) -> usize {
+    let mut sum = 0;
+    for (a, b, c) in input.lines().tuples() {
+        for item in a.chars() {
+            if b.contains(item) && c.contains(item) {
+                sum += to_priority(item);
+                break;
+            }
+        }
+    }
+    sum
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "\
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+";
+
+    #[test]
+    fn part1() {
+        let solution = solve_part1(INPUT);
+        assert_eq!(solution, 157);
+    }
+
+    #[test]
+    fn part2() {
+        let solution = solve_part2(INPUT);
+        assert_eq!(solution, 70);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,6 @@ extern crate aoc_runner;
 
 pub mod day01;
 pub mod day02;
+pub mod day03;
 
 aoc_lib! { year = 2022 }
